c8b386ac77f29f95e163f4adf88bba5765702a46
Sushil patel <sushilx1137@gmail.com>
2025-04-16 11:19:14 +0530

    2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== 'production'
+    ...(process.env.NODE_ENV !== 'production' && process.env.REPL_ID !== undefined
       ? [await import('@replit/vite-plugin-cartographer').then((m) => m.cartographer())]
       : []),
   ],
